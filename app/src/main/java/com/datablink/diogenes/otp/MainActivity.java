2fb6a3edb68d9b07d61c97641bd0e1aca9ba7ee6
Diogenes Leonel <diogenesleonel@gmail.com>
2017-07-30 22:41:34 -0300

    Prototype functions
    - Added Prototype Functions for Button Interaction to get key and delete Data.
    - Added Helper Prototype Functions to load, save and delete QR Code data,
    - Added Helper Prototype Functions to read QR Code and to get OTP number.

--- app/src/main/java/com/datablink/diogenes/otp/MainActivity.java
+++ app/src/main/java/com/datablink/diogenes/otp/MainActivity.java
@@ -2,13 +2,21 @@ package com.datablink.diogenes.otp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
+    Button getKeyButton;
+    Button deleteDataButton;
+    TextView otpNumberText;
+    TextView tokenLabelText;
+
+    class QRCodeData {
+        String key;
+        String label;
     }
 
     @Override
@@ -16,14 +24,95 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        getKeyButton = (Button) findViewById(R.id.getKeyButton);
+        deleteDataButton = (Button) findViewById(R.id.deleteDataButton);
+        otpNumberText = (TextView) findViewById(R.id.otpNumberText);
+        tokenLabelText = (TextView) findViewById(R.id.labelText);
+
+
+
     }
 
+    //--------------------------------- Button Listeners------------------------------------------//
+
+
+    public void getKeyClick(View v){
+
+        Log.d("Clicked", "getKeyClick");
+
+        // Read QRCodeData and save it
+
+        // Show label
+
+        // Generate OTP and show it
+
+
+    }
+
+    public void deleteDataClick(View v){
+
+        Log.d("Clicked", "deleteDataClick");
+
+
+    }
+
+
+
+    //------------------------------------- Helpers ----------------------------------------------//
+
+    public Boolean deleteData(){
+
+        return true;
+
+    }
+
+    public QRCodeData loadData(){
+
+        return new QRCodeData();
+
+    }
+
+    public Boolean saveData(QRCodeData toSave){
+
+        return true;
+
+    }
+
+    public int getOTP(String key){
+
+        // JNI function call
+        int otpNumber = 0;
+
+        return otpNumber;
+
+    }
+
+    public QRCodeData readQRCode(){
+
+        return new QRCodeData();
+
+    }
+
+    public void updateOtpRealTime(){
+
+
+    }
+
+
+    //--------------------------------------- JNI ------------------------------------------------//
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
     public native String stringFromJNI();
+
+
+    // Example of a call to a native method
+    //TextView tv = (TextView) findViewById(R.id.sample_text);
+    //tv.setText(stringFromJNI());
 }
