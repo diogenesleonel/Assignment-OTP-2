b820203cf7758eb7fd6fadbc5761d68cc0e41c70
Diogenes Leonel <diogenesleonel@gmail.com>
2017-08-02 22:30:05 -0300

    Implemented functions to create, delete and retrieve keys.

--- app/src/main/java/com/datablink/diogenes/otp/DataEncryption.java
+++ app/src/main/java/com/datablink/diogenes/otp/DataEncryption.java
@@ -1,5 +1,43 @@
 package com.datablink.diogenes.otp;
 
+import android.content.Context;
+import android.os.Build;
+import android.security.KeyPairGeneratorSpec;
+import android.security.keystore.KeyGenParameterSpec;
+import android.security.keystore.KeyProperties;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.UnrecoverableEntryException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Enumeration;
+import java.util.List;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.CipherInputStream;
+import javax.crypto.CipherOutputStream;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.security.auth.x500.X500Principal;
+
 
 /**
  * Created by diogenes on 02/08/17.
@@ -7,6 +45,124 @@ package com.datablink.diogenes.otp;
  */
 
 public class DataEncryption {
+
+    private KeyStore mKeystore;
+    private Context mContext;
+    private String mAlias;
+    private List<String> keyAliases;
+
+    private  SecretKey mSecretKey;
+    private  KeyPair mKeyPair;
+
+
+
+
+    public DataEncryption(Context context, String alias) {
+
+        mContext = context;
+        mAlias = alias;
+
+        // Get instance of keystore
+        try {
+            mKeystore = KeyStore.getInstance("AndroidKeyStore");
+            mKeystore.load(null);
+        }
+        catch(Exception e) {}
+
+        retrieveKeys();
+
+    }
+
+    public void createNewKeys() {
+
+
+        try {
+
+            // Don't create key if exists
+            if(mKeystore.containsAlias(mAlias))
+                return;
+
+
+            // Generate Symmetric keys
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+
+                // Get keyGenerator instance
+                final KeyGenerator keyGenerator = KeyGenerator
+                        .getInstance(KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore");
+
+                // Setup properties for the key
+                final KeyGenParameterSpec keyGenParameterSpec = new KeyGenParameterSpec.Builder(mAlias,
+                        KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT)
+                        .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
+                        .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
+                        .build();
+
+                keyGenerator.init(keyGenParameterSpec);
+
+                mSecretKey = keyGenerator.generateKey();
+
+
+            } else
+
+                // Generate Asymmetric key for older android versions
+                if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+
+                    Calendar start = Calendar.getInstance();
+                    Calendar end = Calendar.getInstance();
+                    end.add(Calendar.YEAR, 1);
+                    KeyPairGeneratorSpec spec = new KeyPairGeneratorSpec.Builder(mContext)
+                            .setAlias(mAlias)
+                            .setSubject(new X500Principal("CN=OTP, O=Datablink"))
+                            .setSerialNumber(BigInteger.ONE)
+                            .setStartDate(start.getTime())
+                            .setEndDate(end.getTime())
+                            .build();
+
+                    KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA", "AndroidKeyStore");
+                    generator.initialize(spec);
+
+                    mKeyPair = generator.generateKeyPair();
+
+
+                } else {
+
+                    // TODO Lower than 4.3
+                }
+
+        }catch (NoSuchProviderException| NoSuchAlgorithmException|
+                InvalidAlgorithmParameterException e ){
+            e.printStackTrace();
+
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteKey(){
+
+        try {
+            mKeystore.deleteEntry(mAlias);
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Retrieve available keys for current alias
+    private void retrieveKeys(){
+
+        keyAliases = new ArrayList<>();
+        try {
+            Enumeration<String> aliases = mKeystore.aliases();
+
+            while (aliases.hasMoreElements()){
+                keyAliases.add(aliases.nextElement());
+            }
+
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        }
+
+    }
 
 
 }
